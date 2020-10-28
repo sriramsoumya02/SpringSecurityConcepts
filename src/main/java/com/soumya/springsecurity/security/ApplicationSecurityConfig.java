bf89bb21849c1d53b6b5f90b5c83b4ae102300ba
SoumyaSriram <sriramsoumya.02@gmail.com>
2020-10-28 23:13:42 +0100

    created first db user this cant be run becuase password must be encoded

--- src/main/java/com/soumya/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/soumya/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.soumya.springsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -19,5 +24,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
 
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails lakshmi = User.builder()
+                .username("lakshmiDBUser")
+                .password("password")
+                .roles("STUDENT")//ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(lakshmi);
     }
 }
